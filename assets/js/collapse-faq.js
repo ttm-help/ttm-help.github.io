75712a928d11b89924501ae931570479bf260015
Artem Konovalov <izebit@gmail.com>
2022-12-10 11:07:01 +0100

    add favicon with different resolution

--- assets/js/collapse-faq.js
+++ assets/js/collapse-faq.js
@@ -1,9 +1,9 @@
 const coll = document.getElementsByClassName("collapsible");
 for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
+    coll[i].addEventListener("click", function () {
         this.classList.toggle("active");
-        let content = this.nextElementSibling;
-        if (content.style.maxHeight){
+        const content = this.nextElementSibling;
+        if (content.style.maxHeight) {
             content.style.maxHeight = null;
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -2,7 +2,7 @@ const body = document.querySelector('body');
 const menuTrigger = document.querySelector('#toggle-main-menu-mobile');
 const menuContainer = document.querySelector('#main-menu-mobile');
 
-menuTrigger.onclick = function() {
+menuTrigger.onclick = function () {
     menuContainer.classList.toggle('open');
     menuTrigger.classList.toggle('is-active')
     // body.classList.toggle('lock-scroll')
