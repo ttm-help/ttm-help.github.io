7f212517f51cb172ef0c671a2c1384e7ce87e6af
Artem Konovalov <izebit@gmail.com>
2023-01-01 20:23:14 +0100

    optimize js execution

--- assets/js/collapse-faq.js
+++ assets/js/collapse-faq.js
@@ -1,12 +1,15 @@
-const coll = document.getElementsByClassName("collapsible");
-for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        const content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
+window.addEventListener('load', () => {
+        const coll = document.getElementsByClassName("collapsible");
+        for (let i = 0; i < coll.length; i++) {
+            coll[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                const content = this.nextElementSibling;
+                if (content.style.maxHeight) {
+                    content.style.maxHeight = null;
+                } else {
+                    content.style.maxHeight = content.scrollHeight + "px";
+                }
+            });
         }
-    });
-}
+    }
+)
--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -1,9 +1,14 @@
-const body = document.querySelector('body');
-const menuTrigger = document.querySelector('#toggle-main-menu-mobile');
-const menuContainer = document.querySelector('#main-menu');
+const MD_WIDTH = 770;
+if (window.innerWidth < MD_WIDTH) {
+    window.addEventListener('load', () => {
+        const menuTrigger = document.querySelector('#toggle-main-menu-mobile');
+        const menuContainer = document.querySelector('#main-menu');
 
-menuTrigger.onclick = function () {
-    menuContainer.classList.toggle('open');
-    menuTrigger.classList.toggle('is-active')
-    // body.classList.toggle('lock-scroll')
+        menuTrigger.onclick = function () {
+            menuContainer.classList.toggle('open');
+            menuTrigger.classList.toggle('is-active')
+            // body.classList.toggle('lock-scroll')
+        }
+    })
 }
+
