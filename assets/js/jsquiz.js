b5d1a21140f11a7fc3891df261de93280b9dc124
Artem Konovalov <izebit@gmail.com>
2023-01-31 21:39:07 +0100

    add test

---
+++ assets/js/jsquiz.js
@@ -0,0 +1,378 @@
+(function () {
+    const QUESTIONS = [
+        {
+            text: "Данная шкала трихотилломании была разработана в Массачусетском медицинском центре " +
+                "(The Massachusetts General Hospital). Она используется для отслеживания своего состояния.<br>" +
+                "Его можно проходить неоднократно. <br>" +
+                "Результат на сайте покажет общее количество баллов. На психотерапии же мы используем более глубокий " +
+                "анализ - отдельно смотрим на интенсивность побуждений и на фактическое выдергивание волос, " +
+                "а также рассматриваем чувство вины в контексте последствий и его влияния на очередные приступы трихотилломании.<br><br>" +
+                "" +
+                "<blockquote>" +
+                "<a href='https://pubmed.ncbi.nlm.nih.gov/8657844/' rel=\"nofollow\">" +
+                "Keuthen NJ, O’Sullivan RL, Ricciardi JN, et al. The Massachusetts General Hospital (MGH) " +
+                "Hairpulling Scale: 1. Development and Factor Analyses. Psychother Psychosomat 1995; 64: 141–145" +
+                "</a></blockquote>"
+        },
+        {
+            text: "Для каждого вопроса выберите одно утверждение из группы, " +
+                "которое лучше всего описывает ваше поведение и/или чувства на прошлой неделе.<br> " +
+                "Если у вас были спады и усиления симптомов, попробуйте оценить среднее значение за последнюю неделю.<br> " +
+                "Обязательно прочитайте все утверждения в каждой группе, прежде чем сделать свой выбор."
+        },
+        {
+            title: "Частота позывов",
+            question: "Как часто в среднем в день вам хотелось выдернуть волосы?",
+            choices: [
+                {
+                    score: 0,
+                    text: "На этой неделе у меня не было желания выдергивать волосы"
+                },
+                {
+                    score: 1,
+                    text: "На этой неделе я время от времени чувствовал(а) желание выдернуть волосы"
+                },
+                {
+                    score: 2,
+                    text: "На этой неделе я почувствовал(а) желание часто выдергивать волосы"
+                },
+                {
+                    score: 3,
+                    text: "На этой неделе мне очень часто хотелось выдергивать волосы"
+                },
+                {
+                    score: 4,
+                    text: "На этой неделе я чувствовал(а) почти постоянные позывы выдергивать волосы"
+                }
+            ]
+        }, {
+            title: "Интенсивность позывов",
+            question: "Насколько интенсивными или «сильными» были побуждения выдергивать волосы в обычный день?",
+            choices: [
+                {
+                    score: 0,
+                    text: "На этой неделе я не чувствовал(а) никаких позывов выдергивать волосы"
+                },
+                {
+                    score: 1,
+                    text: "На этой неделе я почувствовал(а) легкое побуждение выдергивать волосы"
+                },
+                {
+                    score: 2,
+                    text: "На этой неделе я почувствовал(а) умеренное побуждение выдергивать волосы"
+                },
+                {
+                    score: 3,
+                    text: "На этой неделе я почувствовал(а) сильное желание выдергивать волосы"
+                },
+                {
+                    score: 4,
+                    text: "На этой неделе я почувствовал(а) очень сильное желание выдергивать волосы"
+                }
+            ]
+        },
+        {
+            title: "Способность контролировать побуждения",
+            question: "В среднем в день, насколько вы контролируете желание выдергивать волосы?",
+            choices: [
+                {
+                    score: 0,
+                    text: "На этой неделе я всегда мог(ла) контролировать свои побуждения или я не чувствовал побуждений выдергивать волосы"
+                },
+                {
+                    score: 1,
+                    text: "На этой неделе я смог(ла) отвлечься от побуждений выдергивать волосы большую часть времени"
+                },
+                {
+                    score: 2,
+                    text: "На этой неделе я смог(ла) отвлечься от побуждений выдергивать волосы некоторое время"
+                },
+                {
+                    score: 3,
+                    text: "На этой неделе я редко мог(ла) отвлечься от побуждений выдергивать волосы"
+                },
+                {
+                    score: 4,
+                    text: "На этой неделе я никак не мог(ла) отвлечься от побуждений выдергивать волосы"
+                }
+            ]
+        },
+        {
+            text: "В следующих трех вопросах оценивайте только фактическое выдергивание волос."
+        },
+        {
+            title: "Частота выдергивания волос",
+            question: "Как часто в среднем в день вы на самом деле выдергивали волосы?",
+            choices: [
+                {
+                    score: 0,
+                    text: "На этой неделе я не выдергивал(а) волосы"
+                },
+                {
+                    score: 1,
+                    text: "На этой неделе я время от времени выдергивал(а) волосы"
+                },
+                {
+                    score: 2,
+                    text: "На этой неделе я часто выдергивал(а) волосы"
+                },
+                {
+                    score: 3,
+                    text: "На этой неделе я очень часто выдергивал(а) волосы"
+                },
+                {
+                    score: 4,
+                    text: "На этой неделе я так часто выдергивал(а) волосы, что мне казалось, что я делаю это всегда"
+                }
+            ]
+        },
+        {
+            title: "Попытки сопротивляться выдергиванию волос",
+            question: "Как часто в среднем в день вы пытались удержаться от того, чтобы на самом деле выдергивать волосы?",
+            choices: [
+                {
+                    score: 0,
+                    text: "На этой неделе у меня не было желания выдергивать волосы"
+                },
+                {
+                    score: 1,
+                    text: "На этой неделе я почти все время пытался(ась) сопротивляться желанию выдергивать волосы"
+                },
+                {
+                    score: 2,
+                    text: "На этой неделе я пытался(ась) сопротивляться желанию выдергивать волосы"
+                },
+                {
+                    score: 3,
+                    text: "На этой неделе я редко пытался(ась) сопротивляться желанию выдергивать волосы"
+                },
+                {
+                    score: 4,
+                    text: "На этой неделе я никогда не пытался(ась) сопротивляться желанию выдергивать волосы"
+                }
+            ]
+        },
+        {
+            title: "Контроль за выдергиванием волос",
+            question: "Как часто в среднем в день вам удавалось удержаться от того, чтобы выдергивать волосы?",
+            choices: [
+                {
+                    score: 0,
+                    text: "На этой неделе я не выдергивал(а) волосы"
+                },
+                {
+                    score: 1,
+                    text: "На этой неделе я почти все время мог(ла) сопротивляться тому, чтобы не выдергивать волосы"
+                },
+                {
+                    score: 2,
+                    text: "На этой неделе я большую часть времени мог(ла) сопротивляться тому, чтобы не выдергивать волосы"
+                },
+                {
+                    score: 3,
+                    text: "На этой неделе я иногда сдерживался(ась), чтобы не выдергивать волосы"
+                },
+                {
+                    score: 4,
+                    text: "На этой неделе я редко мог(ла) удержаться от того, чтобы не выдергивать волосы"
+                }
+            ]
+        },
+        {
+            text: "Что касается последнего вопроса, оцените последствия вашего выдергивания волос."
+        },
+        {
+            title: " Ассоциированный дистресс",
+            question: "Выдергивание волос может вызвать у некоторых людей депрессивное состояние, " +
+                "тревожность или грусть.<br>" +
+                "В течение последней недели насколько некомфортно вы чувствовали " +
+                "себя из-за того, что выдергивали волосы?",
+            choices: [
+                {
+                    score: 0,
+                    text: "На этой неделе я не чувствовал(а) себя некомфортно из-за того, что выдергивал волосы"
+                },
+                {
+                    score: 1,
+                    text: "На этой неделе я чувствовал(а) себя немного некомфортно из-за того, что выдергивал волосы"
+                },
+                {
+                    score: 2,
+                    text: "На этой неделе я чувствовал(а) себя некомфортно из-за того, что выдергивал волосы"
+                },
+                {
+                    score: 3,
+                    text: "На этой неделе я чувствовал(а) себя существенно некомфортно из-за того, что выдергивал волосы"
+                },
+                {
+                    score: 4,
+                    text: "На этой неделе я чувствовал(а) интенсивное состояние дискомфорта из-за того, что выдергивал"
+                }
+            ]
+        }
+    ];
+
+
+    let questionCounter = 0;
+    const SELECTIONS = [];
+    const quiz = $('#quiz');
+
+    displayNext();
+
+    $('#next').on('click', function (e) {
+        e.preventDefault();
+
+        if (quiz.is(':animated')) {
+            return false;
+        }
+
+        if (isQuestion(questionCounter)) {
+            saveSelectedOptionTo(SELECTIONS, questionCounter);
+
+            if (isNaN(SELECTIONS[questionCounter])) {
+                $('#warningModal').modal('show')
+            } else {
+                questionCounter++;
+                displayNext();
+            }
+        } else {
+            questionCounter++;
+            displayNext();
+        }
+    });
+
+
+    $('.button').on('mouseenter', function () {
+        $(this).addClass('active');
+    });
+    $('.button').on('mouseleave', function () {
+        $(this).removeClass('active');
+    });
+
+    function createQuestionElement(index) {
+        const qElement = $('<div>', {
+            id: 'question'
+        });
+
+        const header = $(`<h2>${QUESTIONS[index].title}</h2>`);
+        qElement.append(header);
+
+        const question = $('<p>').append(QUESTIONS[index].question);
+        qElement.append(question);
+
+        const radioButtons = createRadios(index);
+        qElement.append(radioButtons);
+
+        return qElement;
+    }
+
+    function createRadios(index) {
+        const radioList = $('<ul>');
+        let item;
+        let input = '';
+        for (let i = 0; i < QUESTIONS[index].choices.length; i++) {
+            item = $('<li style="list-style: none; padding-top: 10px">');
+
+            input = `<input type="radio" id="input-${i}" name="answer" value="${QUESTIONS[index].choices[i].score}" >`;
+            input += `<label for="input-${i}" class="d-inline pl-1">${QUESTIONS[index].choices[i].text}</label>`;
+            item.append(input);
+            radioList.append(item);
+        }
+        return radioList;
+    }
+
+    function saveSelectedOptionTo(array, index) {
+        array[index] = +$('input[name="answer"]:checked').val();
+    }
+
+    function isQuestion(questionCounter) {
+        return QUESTIONS[questionCounter].question !== undefined;
+    }
+
+    function displayNext() {
+        quiz.fadeOut(function () {
+            $('#question').remove();
+
+            if (questionCounter === QUESTIONS.length) {
+                showScore()
+            } else if (isQuestion(questionCounter)) {
+                quiz.append(createQuestionElement(questionCounter)).fadeIn();
+                $('#next').show();
+            } else {
+                const questionTip = QUESTIONS[questionCounter].text;
+                quiz.append(getQuestionTip(QUESTIONS[questionCounter].text)).fadeIn();
+            }
+        });
+    }
+
+    function getQuestionTip(textTip) {
+        const score = $('<p>', {id: 'question'});
+        score.append(textTip);
+        return score;
+    }
+
+    function showScore() {
+
+        const score = $('<p>', {id: 'question'});
+        let totalScore = 0;
+        for (let i = 0; i < SELECTIONS.length; i++) {
+            if (Number.isInteger(SELECTIONS[i]))
+                totalScore += SELECTIONS[i];
+        }
+        const percentage = getPercentage(totalScore);
+
+        score.append($(`<p>У вас <b>${totalScore}</b> баллов.</p>`));
+        score.append($(`<p>${getTipText(percentage)}</p>`));
+        score.append($(`<p>Пройдите этот тест через какой-то промежуток 
+                    времени для того, чтобы определить улучшение или ухудшение своего состояния.</p>`));
+
+        quiz.append(score).fadeIn();
+        $('#next').hide();
+
+        const progressBarDiv = $('#progress-bar');
+        progressBarDiv.append(`
+            <div class="progress mb-1">
+                <div class="progress-bar ${getBarColour(percentage)}" 
+                     role="progressbar" style="width: ${percentage}%" aria-valuenow="${percentage}" aria-valuemin="0"
+                     aria-valuemax="100"></div>
+            </div>`);
+    }
+
+    function getBarColour(percentage) {
+        if (percentage <= 25)
+            return "bg-success";
+        else if (percentage <= 50)
+            return "bg-info";
+        else if (percentage <= 75)
+            return "bg-warning";
+        else
+            return "bg-danger";
+    }
+
+    function getTipText(percentage){
+        if (percentage <= 25)
+            return "Вы отлично справляетесь, но если вам нужна поддержка обратитесь к психотерапевту.";
+        else if (percentage <= 50)
+            return "Рекомендована консультация психотерапевта.";
+        else if (percentage <= 75)
+            return "Рекомендована психотерапия.";
+        else
+            return "Рекомендована психотерапия, а также прием у врача-психиатра для возможного " +
+                "назначения медикаментозного лечения дополнительно к психотерапии.";
+    }
+
+    function getPercentage(score) {
+        let maxScore = 0.0;
+        for (let i = 0; i < QUESTIONS.length; i++) {
+            if (isQuestion(i)) {
+                let current = 0;
+                for (let j = 0; j < QUESTIONS[i].choices.length; j++) {
+                    current = Math.max(current, QUESTIONS[i].choices[j].score);
+                }
+                maxScore += current;
+            }
+        }
+        return (100.0 / maxScore) * score;
+    }
+})();
+
---
+++ test.md
@@ -0,0 +1,36 @@
+---
+title: "Шкала трихотилломании"
+layout: page
+description: "Данный позволяющий следить за изменениями в состоянии лиц, страдающих трихотилломанией"
+date: 2023-01-31
+image: "/assets/img/nguyen-dang-hoang-nhu-qDgTQOYk6B8-unsplash.jpg"
+---
+<div class="modal fade" id="warningModal" tabindex="-1" 
+     role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Чтобы продолжить, выберите вариант ответа
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn" style="background-color: #f36a5d; color: white" data-dismiss="modal">Понятно</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="progress-bar">
+</div>
+
+<div id='quiz'></div>
+<div class='button' id='next' style="padding-left: 25px; padding-right: 25px">
+    <a href='#' style="color: white">Далее</a>
+</div>
+
+<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>
+<script type='text/javascript' src='/assets/js/jsquiz.js'></script>
