ba56e72c2b8435ff307dce8a73ced3438fc7ecc0
Artem Konovalov <izebit@gmail.com>
2025-10-07 14:51:37 -0300

    new version Tue Oct  7 14:51:37 -03 2025

--- assets/js/collapse-faq.js
+++
@@ -1,1 +0,0 @@
-window.addEventListener("load",()=>{const e=document.getElementsByClassName("collapsible");for(let t=0;t<e.length;t++)e[t].addEventListener("click",function(){this.classList.toggle("active");const e=this.nextElementSibling;e.style.maxHeight?e.style.maxHeight=null:e.style.maxHeight=e.scrollHeight+"px"})});
